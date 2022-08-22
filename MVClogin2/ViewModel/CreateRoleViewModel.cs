7d3f12a24bacee830170d2f60a2e5f72d896fe04
MN <mrmaksoracle89537089833@yandex.ru>
2022-08-22 17:44:44 +0300

    add roles

---
+++ MVClogin2/ViewModel/CreateRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVClogin2.ViewModel
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
