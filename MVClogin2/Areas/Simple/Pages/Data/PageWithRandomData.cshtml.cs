6ac2e5e35a9f9ea17164a0defcb642fefad0d8ec
MN <mrmaksoracle89537089833@yandex.ru>
2022-08-25 11:56:34 +0300

    End with refreshing dataEnd with refreshing data

--- MVClogin2/Areas/Simple/Pages/Data/PageWithRandomData.cshtml.cs
+++ MVClogin2/Areas/Simple/Pages/Data/PageWithRandomData.cshtml.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using MVClogin2.Models;
 using MVClogin2.Services;
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
-using System.Reflection.Emit;
 using System.Threading;
-using System.Timers;
-using Timer = System.Threading.Timer;
 
 namespace MVClogin2.Areas.Simple.Pages.Data
 {
@@ -18,7 +11,8 @@ namespace MVClogin2.Areas.Simple.Pages.Data
     {
         public string data { get; set; }
         public readonly IWebHostEnvironment _env;
-        
+
+
         public PageWithRandomDataModel(IWebHostEnvironment env)
         {
             _env = env;
@@ -26,17 +20,11 @@ namespace MVClogin2.Areas.Simple.Pages.Data
 
         public void OnGet()
         {
-            var stateTimer = new Timer(MyMethod, null, 200, 1000);
-            ViewData["path"] = Path.Combine(_env.WebRootPath, "Data json", "RandomData.json");
-            ViewData["c"] = (new JsonFileRandomDataService(_env)).GetData().data;
-            data = (new JsonFileRandomDataService(_env)).GetData().data;
+            ViewData["path"] = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            Console.WriteLine(ViewData["path"]);
+
         }
 
-        private void MyMethod(object state)
-        {
-            ViewData["c"] = (new JsonFileRandomDataService(_env)).GetData().data;
-            Console.WriteLine(ViewData["c"]);
-            data = (new JsonFileRandomDataService(_env)).GetData().data;
-        }
+
     }
 }
